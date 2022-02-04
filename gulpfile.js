f1aa9d4ed6ce0694f630319b5eba97df7402ca7f
Alex Linnik <a.v.linnik@gmail.com>
2022-02-04 16:38:15 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
+// Add reference to MS Engine that repleces all external names (localization files too)
 const hackedExternals = require("@microsoft/sp-build-core-tasks/lib/webpack/LegacyExternals");
+
 const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
@@ -8,7 +10,7 @@ build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not came
 build.configureWebpack.mergeConfig({
     additionalConfiguration: function (cfg) {
         console.log("WEBPACK CONFIG MODIFICATION - REMOVE CONTENT HASH");
-        console.log(cfg.output.chunkFilename, cfg.output.filename);
+        // Set main files untouched or we can add package versions here
         cfg.output.chunkFilename = cfg.output.chunkFilename.replace("_[contenthash]", "");
         cfg.output.filename = cfg.output.filename.replace("_[contenthash]", "");
         console.log(cfg.output.chunkFilename, cfg.output.filename);
@@ -17,11 +19,12 @@ build.configureWebpack.mergeConfig({
             singleLocale: build.configureWebpack.taskConfig.singleLocale,
             configJson: build.configureWebpack.properties.configJson,
             buildFolder: build.configureWebpack.buildConfig.rootPath,
-            serveMode: true
+            serveMode: true // This option leave all names untouched
         });
 
         for (const plugin of cfg.plugins) {
             if (plugin.constructor.name == "ManifestPlugin") {
+                // To add versions to external files, internal function must be rewritten
                 legacyExternals.updateWebpackConfiguration(cfg);
             }
         }
